b07f5227930c1eb863c09f5ce65f258b19889931
sassembla <sassembla@hotmail.com>
2012-08-17 15:14:29 +0900

    update TEST_EXEC_ADDED

--- src/com/kissaki/mondogrosso/mondogrossoProcessOrders/MondogrossoCaseClasses.scala
+++ src/com/kissaki/mondogrosso/mondogrossoProcessOrders/MondogrossoCaseClasses.scala
@@ -2,8 +2,25 @@ package com.kissaki.mondogrosso.mondogrossoProcessOrders
 
 
 
-trait MondogrossoCaseClasses {
+trait MondogrossoCaseClasses {}
 
-}
+//サンプル
+case class MondogrossoProcessOrderSample(a :String) extends MondogrossoCaseClasses
 
-case class MondogrossoProcessOrder(a :String) extends MondogrossoCaseClasses
+//GWTのテストの開始信号を受ける
+case class EXEC_INPUT_GWTTEST(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
+
+
+//特定ファイルの監視開始
+case class EXEC_START_OBSERVE(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
+
+//監視終了
+case class EXEC_END_OBSERVE(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
+
+case class EXEC_START_TESTRUN(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
+case class EXEC_TESTFINISHED_TESTRUN(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
+case class EXEC_END_TESTRUN(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
+
+case class EXEC_START_PROCESS(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
+case class EXEC_KILL_PROCESS(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
+case class EXEC_END_PROCESS(nbr1: Int, nbr2: Int) extends MondogrossoCaseClasses
